46dd29a2a13d187221a072fc4f2b809a86a73e3b
Mubarak A Ibrahim <imubarak2424@gmail.com>
2022-02-15 18:05:13 +0100

    Delete queries.sql

--- queries.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-01-2019';
-SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-FROM animals WHERE neutered = TRUE;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
