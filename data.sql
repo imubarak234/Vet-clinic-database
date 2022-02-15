4fd1b743c737db546326b17eb44eded9ef4ec25c
Mubarak A Ibrahim <imubarak2424@gmail.com>
2022-02-16 00:24:04 +0100

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO animals VALUES (1, 'Agumon', '02-03-2020', 0, TRUE, 10.23);
+INSERT INTO animals VALUES (2, 'Gabumon', '15-11-2018', 2, TRUE, 8);
+INSERT INTO animals VALUES (3, 'Pikachu', '07-01-2021', 1, FALSE, 15.04);
+INSERT INTO animals VALUES (4, 'Devimon', '12-05-2017', 5, TRUE, 11);
