66d50a1279da7948ceb3bb2f70cc6b916e580bce
Mubarak A Ibrahim <imubarak2424@gmail.com>
2022-02-16 11:50:36 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,7 @@ INSERT INTO animals VALUES (1, 'Agumon', '02-03-2020', 0, TRUE, 10.23);
 INSERT INTO animals VALUES (2, 'Gabumon', '15-11-2018', 2, TRUE, 8);
 INSERT INTO animals VALUES (3, 'Pikachu', '07-01-2021', 1, FALSE, 15.04);
 INSERT INTO animals VALUES (4, 'Devimon', '12-05-2017', 5, TRUE, 11);
+
 INSERT INTO animals VALUES (5, 'Charmander', '08-02-2020', 0, FALSE, -11);
 INSERT INTO animals VALUES (6, 'Plantmon', '15-11-2020', 2, TRUE, -5.7);
 INSERT INTO animals VALUES (7, 'Squirtle', '02-04-1993', 3, FALSE, -12.13);
